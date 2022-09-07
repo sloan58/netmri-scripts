0e2e0569a0deff1e53f97ac4f5b806681c428baa
Marty Sloan <marty@karmatek.io>
2022-09-07 14:28:09 -0400

    Add GetPerformanceData

---
+++ GetPerformanceData.py
@@ -0,0 +1,60 @@
+import sys
+from pprint import pprint
+
+import requests
+from infoblox_netmri import InfobloxNetMRI
+
+chart_start_time = '2022-09-07'  # Begin time for chart data
+chart_end_time = '2022-09-08'    # Begin time for chart data
+
+host = ''      # NetMRI IP Address/FQDN
+username = ''  # NetMRI Username
+password = ''  # NetMRI Password
+
+# Chart data to collect
+chart_names = [
+    'basic_configuration',
+    'load_average',
+    'load_per_comp',
+    'mem_consumption',
+    'swap_consumption',
+    'mem_per_comp',
+    'total_iowait',
+    'read_write',
+    'partition_cap',
+]
+
+try:
+    net_mri_client = InfobloxNetMRI(
+        host=host,
+        username=username,
+        password=password
+    )
+except requests.exceptions.ConnectionError as e:
+    print('Could not connect to NetMRI')
+    sys.exit()
+
+"""
+Docs:
+    DataSourceBroker: /netmri/share/api/pythondoc/v3_8_0.DataSourceBroker.html
+    DataSource Object Model: /api/3.8/data_sources/model
+"""
+data_source_broker = net_mri_client.get_broker('DataSource')
+
+"""
+Docs:
+    PerformanceDataBroker: /netmri/share/api/pythondoc/v3_8_0.PerformanceDataBroker.html
+"""
+performance_broker = net_mri_client.get_broker('PerformanceData')
+
+for source in data_source_broker.index():
+    for chart_name in chart_names:
+        chart = {
+            'chart': chart_name,
+            'starttime': chart_start_time,
+            'endtime': chart_end_time,
+            'unit_id': source.DataSourceID
+        }
+        data = performance_broker.get_chart_data(**chart)
+        pprint('##### ' + chart_name + ' #####')
+        pprint(data)
