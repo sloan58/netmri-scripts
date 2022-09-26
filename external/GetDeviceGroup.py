c29c6beb97a577c190278a71aad007a517755d0b
Marty Sloan <marty@karmatek.io>
2022-09-26 12:27:04 -0400

    Add MigrateGroupDefn create process

---
+++ external/GetDeviceGroup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import requests
+from dotenv import load_dotenv
+from infoblox_netmri import InfobloxNetMRI
+
+load_dotenv()
+
+
+try:
+    net_mri_client = InfobloxNetMRI(
+        host=os.getenv('NETMRI_HOST'),
+        username=os.getenv('NETMRI_USER'),
+        password=os.getenv('NETMRI_PASSWORD')
+    )
+except requests.exceptions.ConnectionError as e:
+    print('Could not connect to NetMRI')
+    sys.exit()
+
+
+group_broker = net_mri_client.get_broker('DeviceGroupDefn')
+
+group = group_broker.search(**{
+    'GroupName': 'TestFromApi'
+})
+
+print(vars(group[0]))
--- external/MigrateGroupDefn.py
+++ external/MigrateGroupDefn.py
@@ -31,17 +31,45 @@ group_broker_dst = net_mri_client_dst.get_broker('DeviceGroupDefn')
 group_list = []
 created_groups = []
 
+
+def fetch_new_parent_id(parent_group):
+    return group_broker_dst.search(**{
+        'GroupName': parent_group.GroupName
+    })[0].GroupID
+
+
+def set_parent_group_id(group):
+    parent_group_id = group.ParentDeviceGroupID
+    if group.ParentDeviceGroupID != 0:
+        parent_group = list(filter(lambda pgroup: pgroup.GroupID == group.ParentDeviceGroupID, group_list))[0]
+        parent_group_id = fetch_new_parent_id(parent_group)
+    return parent_group_id
+
+
+def prepare_group_params(group):
+    group.ParentDeviceGroupID = set_parent_group_id(group)
+    group_dict = vars(group)
+    del group_dict['broker']
+    return group_dict
+
+
 def create_group(group):
-    print(f'Processing group {group.GroupName} with group id {group.GroupID} and parent group id {group.ParentDeviceGroupID}')
-    if group.ParentDeviceGroupID == 0 or (group.ParentDeviceGroupID in created_groups and group.GroupID not in created_groups):
-        print(f'Group either has parent id 0 or parent group was already created.  Creating this group now.')
-        print(f'Appending to created_groups list')
+    print(
+        f'Processing group {group.GroupName} with group id {group.GroupID}.  '
+        f'Parent group id is {group.ParentDeviceGroupID}'
+    )
+    if group.ParentDeviceGroupID == 0 \
+            or (group.ParentDeviceGroupID in created_groups and group.GroupID not in created_groups):
+        print(f'Group {group.GroupName} is ready to be created.')
+        group_dict = prepare_group_params(group)
+        group_broker_dst.create(**group_dict)
         created_groups.append(group.GroupID)
-        print(created_groups)
+        print(f'Created group {group.GroupName} and appended to created_groups list.')
 
     else:
-        print(f'Parent group {group.ParentDeviceGroupID} does not exist.  Creating now')
+        print(f'Group {group.GroupName} is NOT ready to be created.  Creating parent group first.')
         parent_group = list(filter(lambda pgroup: pgroup.GroupID == group.ParentDeviceGroupID, group_list))[0]
+        print(f'Creating parent group {parent_group.GroupName}.')
         create_group(parent_group)
 
 
